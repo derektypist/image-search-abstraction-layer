e298714ce1ed28f291a23554043a36159afbcf84
Glitch (rainbow-best-exhaust) <none>
2021-07-12 14:01:15 +0000

    ❤️🍪 Checkpoint ./server.js:28918765/176 ./package.json:28918765/1420

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 
 // we've started you off with Express (https://expressjs.com/)
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
+require('dotenv').config();
 const express = require("express");
 const app = express();
 let mongo = require('mongodb').MongoClient;
@@ -19,6 +20,8 @@ const dreams = [
 // make all the files in 'public' available
 // https://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 // https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
