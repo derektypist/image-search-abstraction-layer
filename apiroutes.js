43272b250f06b20de427b7a8e0d0e1be51f2458f
Glitch (rainbow-best-exhaust) <none>
2021-07-21 11:33:44 +0000

    🌉⛰ Checkpoint ./apiroutes.js:28918765/391

--- apiroutes.js
+++ apiroutes.js
@@ -18,13 +18,28 @@ module.exports = function(app) {
     
     console.log('Successfully connected to database');
     
+    
+    // Get JSON Response
+    
+    app.get('/imagesearch/:search',(req,res) => {
+      let search = req.params.search;
+    });
     // Search for top 10
     app.get('/latest/imagesearch',(req,res) => {
             searchQueryModel.find({},null,{sort:{_id:-1},limit:10},(err,docs) => {
               if (err) {
-                co
+                console.log(err);
               }
+              
+              res.json(docs);
             });
-            });
+          });
+    
+    // POST Method
+    app.post('/api/imagesearch', (req,res) => {
+      let search = req.body.search;
+      search = encodeURI(search);
+      let page = req.body.page ? req.body.page:1;
+    });
   });
 };
