fdb3273067ea7ffbdd13f9168bb12be4f30ff4c3
Glitch (rainbow-best-exhaust) <none>
2021-07-23 12:04:21 +0000

    👣👲 Checkpoint ./server.js:28918765/1644 ./apiroutes.js:28918765/1629

--- server.js
+++ server.js
@@ -5,20 +5,11 @@
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
 const app = express();
-let mongoose = require('mongoose');
-let axios = require('axios');
 const bodyParser = require('body-parser');
-let request = require('request');
 
 
-const Schema = mongoose.Schema;
 
-// Create Instance of Schema
-let searchQuerySchema = new Schema({
-  images: String
-});
-
-let searchQueryModel = mongoose.model('searchQueryModel',searchQuerySchema);
+require('./apiroutes');
 // make all the files in 'public' available
 // https://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
@@ -32,51 +23,7 @@ app.get("/", (request, response) => {
 });
 
 
-mongoose.connect(process.env.DB, {useNewUrlParser:true, useUnifiedTopology:true}, (err) => {
-    if (err) {
-      console.log(err);
-    }
-    
-    console.log('Successfully connected to database');
-    
-    
-    // Get JSON Response
-    
-    app.get('/api',(req,res) => {
-      let search = req.query.search;
-      let page = req.query.offset? req.query.offset:1;
-      let ggle = `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=017576662512468239146:omuauf_lfve&q=${search}&searchType=image&imgType=photo`;
-      axios.get(ggle).then(data => {
-        res.json(data.data.items);
-      }).catch(err => {
-        res.send("Error in fetching the images");
-      });
-    });
-    
-    // Search for top 10
-    app.get('/data',(req,res) => {
-            searchQueryModel.find({},null,{sort:{_id:-1},limit:10},(err,docs) => {
-              if (err) {
-                console.log(err);
-              }
-              
-              res.json(docs);
-            });
-          });
-    
-    // POST Method
-    app.post('/data', (req,res) => {
-      let doc = new searchQueryModel({images:req.body.searchQuery});
-      doc.save((err) => {
-        if (err) {
-          console.log(err);
-        }
-        console.log("Saved successfully");
-        res.send("Posted successfully");
-      });
-      
-    });
-  });
+
 
 // 404 Not Found Middleware
 app.use(function(req,res,next) {
