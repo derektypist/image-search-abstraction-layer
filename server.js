8f3d00812e1e3f62d40b146c46170a861f104afa
Glitch (rainbow-best-exhaust) <none>
2021-07-26 11:35:38 +0000

    🎆😬 Checkpoint ./server.js:28918765/546

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const axios = require('axios');
+const request = require('request');
 
 const Schema = mongoose.Schema;
 
@@ -39,7 +40,12 @@ app.get('/imagesearch/:search',(req,res) => {
       let search = req.query.search;
       let page = req.query.offset? req.query.offset:1;
       let ggle = `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=017576662512468239146:omuauf_lfve&q=${search}&searchType=image&imgType=photo`;
-      searchQueryModel.findOne();
+      searchQueryModel.findOne({term:search,searched_on:new Date().toUTCString()}, (err,doc) => {
+        request(ggle,{json:true},(err,red,data) => {
+          let dat=data.items.map((i) => {return{image_url:i.link,alt_text:i.snippet,page_url:i.image.contextLink}});
+          dat=d
+        });
+      });
     });
     
 // Search for top 10
